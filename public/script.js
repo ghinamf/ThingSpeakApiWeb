c5e52ae69ded8002190d98f0f8d30efe6a86f9cc
ghinamf <mufidahg17@gmail.com>
2023-03-19 19:21:26 +0700

    hello

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 // api url
-const api_url = "https://api.thingspeak.com/channels/<ChannelID>/feeds.json?api_key=<ReadAPIKeys>&results=1";
+const api_url = "https://api.thingspeak.com/channels/2057294/feeds.json?api_key=XKP7EW6NZH2FQVT6&results=1";
 // masukan url sesuai dengan format <ChannelID>, <ReadAPIKeys> yang ada di Thingspeak, tanpa menggunakan < >
 
 setInterval(() => {
@@ -9,6 +9,6 @@ setInterval(() => {
       var field = JSON.stringify(res.feeds[0]);
       var obj = JSON.parse(field);
       document.getElementById("field-1").innerHTML = obj.field1;
-      document.getElementById("field-2").innerHTML = obj.field2;
+      document.getElementById("LDR").innerHTML = obj.field2;
     });
 }, 1000);
